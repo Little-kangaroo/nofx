73c0dadda630c5abdce4bc3d8d144dc276a57c13
北海 <beihai@webuy.ai>
2025-11-11 11:29:33 +0800

    fix

--- web/src/components/landing/AboutSection.tsx
+++ web/src/components/landing/AboutSection.tsx
@@ -108,8 +108,6 @@ export default function AboutSection({ language }: AboutSectionProps) {
               />
             </div>
           </div>
-        </div>
-      </div>
     </AnimatedSection>
   )
 }
